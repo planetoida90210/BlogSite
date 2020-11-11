0dcf31f555509956c3dab82b531359969305352b
planetoida90210 <galeriakoloru90210@gmail.com>
2020-11-11 22:37:36 +0100

    add admin login

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const cookieSession = require('cookie-session');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const config = require('./config')
 
-var indexRouter = require('./routes/index');
-var newsRouter = require('./routes/news');
-var quizRouter = require('./routes/quiz');
-var adminRouter = require('./routes/admin');
+const indexRouter = require('./routes/index');
+const newsRouter = require('./routes/news');
+const quizRouter = require('./routes/quiz');
+const adminRouter = require('./routes/admin');
 
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +23,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name: 'session',
+  keys: config.keySession,
+  maxAge: config.maxAgeSession
+}))
 
 app.use(function(req, res, next) {
   res.locals.path = req.path
