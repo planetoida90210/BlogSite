a339b60ec6db3115153ceba9c66e05894a631cc8
planetoida90210 <galeriakoloru90210@gmail.com>
2020-11-13 23:46:38 +0100

    fixed bug in admin code

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,14 @@ const { NotExtended } = require('http-errors');
 const router = express.Router();
 
 router.all('*', (req, res, next) => {
-  if(!req.session.admin){
-    res.redirect('login')
+  if (!req.session.admin) {
+    res.redirect('login');
 
     return;
   }
 
-  next()
-})
+  next();
+});
 
 /* GET home page. */
 router.get('/',(req, res ) => {
